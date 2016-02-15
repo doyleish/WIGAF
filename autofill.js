62570763cef50d7655e3284ff7080e7740ed2cac
Ryan Doyle <rcdoyle@mtu.edu>
2016-02-15 13:48:32 -0500

    its working. tears

--- autofill.js
+++ autofill.js
@@ -1,19 +1,26 @@
 var CLIENT_ID = '730100849179-n047mcmlc01nc9164e44334v4lmbeukn.apps.googleusercontent.com';
 var SCOPE = "https://www.googleapis.com/auth/calendar.readonly";
 
-var days_raw = document.getElementsByClassName("dateHeader weekday");
-var times_raw = document.getElementsByClassName("proposed");
-var num_times = times_raw.length; //number of times per day(including blanks)
-var times_by_id = [num_times+1];
 
-times_by_id[0] = "";
-for(i=1;i<=num_times;i++){
-    times_by_id[i] = (times_raw[i-1].id);
+
+/**
+ * Analyzing the total number of times and
+ * obtaining availability in a blocking fashion
+ */
+var days_raw = document.getElementsByClassName("dateHeader weekday");
+times_raw = document.getElementsByClassName("proposed");
+times_by_id = new Array(); //starting these at 1 to prevent js thinking I'm obtaining .toString on a whole array
+return_val_req = 0;
+
+times_by_id.push("");
+for(i=1;i<=times_raw.length;i++){
+    times_by_id.push(times_raw[i-1].id);
 }
 console.log(times_by_id);
-
-var num_days = days_raw.length; //number of days shown
 console.log(times_by_id.length);
+console.log(document.getElementById(times_by_id[1]));
+
+
 
 /**
  * Check if current user has authorized this application.
@@ -63,59 +70,62 @@ function loadCalendarApi() {
 }
 
 function autofill() {
-    
-    var calendars = [{}];
-    
-    var request = gapi.client.calendar.calendarList.list({});
-    request.execute(function(resp){
-        var len = resp['items'].length;
-        for(i=0;i<len;i++){
-            if(resp['items'][i]['accessRole'] == "owner"){
-                calendars.push({'id': resp['items'][i]['id']});
-            }
-        }
-    })
-
-    console.log(calendars);
-    
-
     for(i=1;i<times_by_id.length;i++){
-        var current_time = document.getElementById(times_by_id[i].toString());
-        var current_id = times_by_id[i].toString();
-        console.log(i+ " " + current_id);
-        var current_day = days_raw[i%num_days];
-        var weekday = current_day.childNodes[1].innerText;
-        var date = current_day.childNodes[3].innerText;
-        var month = current_day.childNodes[5].innerText;
-        var year = "2016";
-        var time = current_time.childNodes[1].innerText;
-        if(time==""){break;}
-        console.log(dateify(weekday, date, month, year, time, false));
-        console.log(dateify(weekday, date, month, year, time, true));
-        var date1 = new Date(dateify(weekday, date, month, year, time, false)).toISOString();
-        var date2 = new Date(dateify(weekday, date, month, year, time, true)).toISOString();
-        
-                                                                                                     //   for(j=1;j<calendars.length;j++){
-            request = gapi.client.calendar.freebusy.query({
-                'timeMin': date1,
-                'timeMax': date2,
-                'timeZone': 'EST',
-                'items': [
-                    {'id':'rcdoyle@mtu.edu'}
-                
-                ]
-            });
-            request.execute(function(resp) {
-                for (x in resp['calendars']){
-                    if(resp['calendars'][x]['busy'].length == 0){
-                        document.getElementById(times_by_id[i]).className="canDo";
-                    }
-                }
-            })
-                                                                                                          //      if(busy){break;}
-        console.log(i);                                                                                                  //  }
-
+        doAll(i);
     }
+}
+
+function doAll(i){
+    //var temparr = [];
+    var current_id;
+    var current_day;
+    var weekday;
+    var date;
+    var month;
+    var year;
+    var time;
+    var date1;
+    var date2;
+    var request;
+    var value;
+    
+    current_time = document.getElementById(times_by_id[i]);
+    current_id = times_by_id[i];
+    console.log(i+ " " + current_id);
+    current_day = days_raw[i%(days_raw.length)];
+    weekday = current_day.childNodes[1].innerText;
+    date = current_day.childNodes[3].innerText;
+    month = current_day.childNodes[5].innerText;
+    year = "2016";
+    time = current_time.childNodes[1].innerText;
+    if(time==""){return;}
+    console.log(dateify(weekday, date, month, year, time, false));
+    date1 = new Date(dateify(weekday, date, month, year, time, false)).toISOString();
+    date2 = new Date(dateify(weekday, date, month, year, time, true)).toISOString();
+
+    request = gapi.client.calendar.freebusy.query({
+        'timeMin': date1,
+        'timeMax': date2,
+        'timeZone': 'EST',
+        'items': [
+            {'id':'rcdoyle@mtu.edu'}
+        ]
+    });
+    request.execute(function(resp) {
+        var retval;
+        for (x in resp['calendars']){
+            retval = resp['calendars'][x]['busy'].length;
+        }
+
+        afterExec(i, retval);
+    });
+    
+}
+
+
+function afterExec(id, bool){
+    console.log(id + " asdf " + bool);
+    if(bool == 0){ document.getElementById(times_by_id[id]).className = "canDo"; }
 }
 
 function dateify(weekday, date, month, year, time, offset){
